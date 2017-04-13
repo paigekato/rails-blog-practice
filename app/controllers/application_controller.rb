9539a1a43c93a8573003fed2b7238de8260e3a10
Paige <paigekato@gmail.com>
2017-04-13 00:25:52 +0000

    Done Blog practice for Rails

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+  end
   protect_from_forgery with: :exception
 end
